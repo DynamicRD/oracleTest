76455f5418390158c09dfe3e0518ac1ba3e62707
DynamicRD <qlfflgkfflxjs@gmail.com>
2024-11-15 10:37:57 +0900

    PL/SQL DEPARTMENTS 테이블에 전체 내용을 CURSOR 저장하고 FETCH해서 전체정보를 출력 테스트

--- hr1115_plsql.sql
+++ hr1115_plsql.sql
@@ -1,11 +1,19 @@
--- departments 에서 반복문을 이용해서 부서 10,20,30,40 정보를 출력하시오
-
-begin
-     for i in 2..9 loop
-            dbms_output.put_line('---------'||i||'단---------');
-            for j in 1..9 loop
-                dbms_output.put_line(i || ' X ' || j || ' = ' || i*j);
-            end loop;
-     end loop;
-end;
+-- PL/SQL DEPARTMENTS 테이블에 전체 내용을 CURSOR 저장하고 FETCH해서 전체정보를 출력하시오
+DECLARE
+        VDEPARTMENTS DEPARTMENTS%ROWTYPE;
+        CURSOR C1 IS SELECT * FROM DEPARTMENTS;
+BEGIN
+        /*
+        OPEN C1;
+        LOOP
+                FETCH C1 INTO VDEPARTMENTS;
+                EXIT WHEN C1%NOTFOUND;
+                DBMS_OUTPUT.PUT_LINE(VDEPARTMENTS.DEPARTMENT_ID||' / '||VDEPARTMENTS.DEPARTMENT_NAME);
+        END LOOP;
+        CLOSE C1;
+        */
+        FOR VDEPARTMENTS IN C1 LOOP
+            DBMS_OUTPUT.PUT_LINE(VDEPARTMENTS.DEPARTMENT_ID||' / '||VDEPARTMENTS.DEPARTMENT_NAME);
+        END LOOP;
+END;
 /
