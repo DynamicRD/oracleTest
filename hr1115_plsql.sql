916b3e38d47614d937ad5d5843f0dc24dd3c340b
DynamicRD <qlfflgkfflxjs@gmail.com>
2024-11-15 10:16:30 +0900

    PL/SQL FOR IN LOOP 구구단 테스트

--- hr1115_plsql.sql
+++ hr1115_plsql.sql
@@ -1,12 +1,11 @@
 -- departments 에서 반복문을 이용해서 부서 10,20,30,40 정보를 출력하시오
-select * from departments where department_id in(10,20,30,40);
 
-declare
-    row_type departments%rowtype;
 begin
-    for i in 1..9 loop
-            select * into row_type from departments where department_id = i*10;
-            dbms_output.put_line(row_type.department_id || ' ' ||row_type.department_name);
-    end loop;
+     for i in 2..9 loop
+            dbms_output.put_line('---------'||i||'단---------');
+            for j in 1..9 loop
+                dbms_output.put_line(i || ' X ' || j || ' = ' || i*j);
+            end loop;
+     end loop;
 end;
 /
