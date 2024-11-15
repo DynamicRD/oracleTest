fd15820c5f8acf46ec84978649cd3300aa342fb0
DynamicRD <qlfflgkfflxjs@gmail.com>
2024-11-15 11:39:44 +0900

    PL/SQL EMPLOYEES 테이블에서 요구한 부서별로 사용사 정보(이름,월급)을 CURSOR에 저장하고 부서별 요청시 해당되는 부서 정보를 출력 테스트

--- hr1115_plsql.sql
+++ hr1115_plsql.sql
@@ -1,19 +1,35 @@
--- PL/SQL DEPARTMENTS 테이블에 전체 내용을 CURSOR 저장하고 FETCH해서 전체정보를 출력하시오
+-- PL/SQL EMPLOYEES 테이블에서 요구한 부서별로 사용사 정보(이름,월급)을 CURSOR에 저장하고 부서별 요청시 해당되는 부서 정보를 출력하시오 
+
+SELECT * FROM EMPLOYEES WHERE DEPARTMENT_ID = 30;
+
 DECLARE
-        VDEPARTMENTS DEPARTMENTS%ROWTYPE;
-        CURSOR C1 IS SELECT * FROM DEPARTMENTS;
+        VEMPROW_TYPE EMPLOYEES%ROWTYPE;
+        VSALARY VARCHAR2(10);
+        VNO NUMBER(3);
+        --부서별로 정보를 저장할 수 있는 커서생성
+        CURSOR C1 (VDEPTNO EMPLOYEES.DEPARTMENT_ID%TYPE)
+        IS 
+        SELECT * FROM EMPLOYEES WHERE DEPARTMENT_ID = VDEPTNO;
 BEGIN
-        /*
-        OPEN C1;
-        LOOP
-                FETCH C1 INTO VDEPARTMENTS;
-                EXIT WHEN C1%NOTFOUND;
-                DBMS_OUTPUT.PUT_LINE(VDEPARTMENTS.DEPARTMENT_ID||' / '||VDEPARTMENTS.DEPARTMENT_NAME);
+        --부서별 정보를 생성시킨다. 랜덤값으로
+        VNO := ROUND(DBMS_RANDOM.VALUE(10,110),-1);
+        --부서번호 40번호 종료해라.
+        IF (VNO = 40) THEN
+                DBMS_OUTPUT.PUT_LINE(VNO || '해당되지 않는 부서 번호입니다.');
+                RETURN;
+        END IF;
+        --부서번호 정보를 가져와서 월급에 대해서 평가를 진행한다
+        FOR VEMPROW_TYPE IN C1(VNO) LOOP
+                IF VEMPROW_TYPE.SALARY BETWEEN 1 AND 1000 THEN
+                    VSALARY := '낮음';
+                ELSIF VEMPROW_TYPE.SALARY BETWEEN 1000 AND 2000 THEN
+                    VSALARY := '중간';
+                ELSIF VEMPROW_TYPE.SALARY BETWEEN 2000 AND 3000 THEN
+                    VSALARY := '높음';
+                ELSE
+                    VSALARY := '최고';
+                END IF;
         END LOOP;
-        CLOSE C1;
-        */
-        FOR VDEPARTMENTS IN C1 LOOP
-            DBMS_OUTPUT.PUT_LINE(VDEPARTMENTS.DEPARTMENT_ID||' / '||VDEPARTMENTS.DEPARTMENT_NAME);
-        END LOOP;
+        DBMS_OUTPUT.PUT_LINE(VEMPROW_TYPE.FIRST_NAME || ' / ' || VEMPROW_TYPE.SALARY || ' / ' || VSALARY);
 END;
 /
