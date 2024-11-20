c32d72080253782db63f26a73d26373a2f219ce2
DynamicRD <qlfflgkfflxjs@gmail.com>
2024-11-20 17:47:54 +0900

    수정

--- hr1120_CallableStatement.sql
+++ hr1120_CallableStatement.sql
@@ -134,3 +134,38 @@ VARIABLE MESSAGE VARCHAR2(200);
 EXECUTE BOOKS_PROCEDURE(1, 10000, :MESSAGE);
 
 SELECT * FROM BOOKS;
+-------------------
+-- employees
+SELECT * FROM EMPLOYEES;
+desc employees;
+
+--books
+DROP TABLE BOOKS;
+CREATE TABLE books (
+       id number(4), 
+        title varchar2(50), 
+        publisher varchar2(30), 
+        year varchar2(10), 
+        price number(6) 
+);
+ALTER TABLE BOOKS ADD CONSTRAINT BOOKS_ID_PK PRIMARY KEY(ID);
+
+CREATE SEQUENCE BOOKS_ID_SEQ
+START WITH 1
+INCREMENT BY 1;
+
+INSERT INTO books  VALUES (bookS_id_seq.nextval, 'Operating System Concepts', 'Wiley', '2003',30700);
+INSERT INTO books  VALUES (bookS_id_seq.nextval, 'Head First PHP and MYSQL', 'OReilly', '2009', 58000);
+INSERT INTO books  VALUES (bookS_id_seq.nextval, 'C Programming Language', 'Prentice-Hall', '1989', 35000);
+INSERT INTO books  VALUES (bookS_id_seq.nextval, 'Head First SQL', 'OReilly', '2007', 43000);
+commit;
+
+SELECT * FROM BOOKS;
+
+DESC BOOKS;
+SELECT * FROM USER_CONS_COLUMNS WHERE TABLE_NAME = 'BOOKS';
+
+DELETE FROM BOOKS WHERE ID = 5;
+ROLLBACK;
+UPDATE BOOKS SET TITLE = 'KKK', PUBLISHER = 'JAVA',YEAR ='2024',PRICE=33000 WHERE ID = 10;
+------------------------------------
